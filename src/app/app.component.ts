5ce0f99291e57e1e1c414e91125ce003d8e34c51
Juan Pablo Chica <juanpchica@hotmail.com>
2018-06-05 22:25:51 -0500

    Function add new student added with forms bootstrap 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = ' my app 3';
+
+  students : any = [
+  	{id:1,name:"Juan Pablo",age:23},
+  	{id:2,name:"Luis Felipe",age:26},
+  	{id:3,name:"Nicolas",age:9},
+  	{id:4,name:"Fernando",age:15},
+  	{id:5,name:"Carlos",age:28},
+  	{id:6,name:"Maria",age:18}
+  ];
+
+  student:any = {};
+
+
+  validateFieldsRequired(){
+  	if (this.student == {}) {
+  		console.log("Campos vacios");
+  	}
+  }
+  addStudent(){
+
+  	//Valido los campos antes de agregar el nuevo estudiante
+  	this.validateFieldsRequired();
+
+  	// Agrego student a students
+  	this.students.push(this.student);
+  	this.student = {};  //Vacio nuevamente el objeto
+
+  }
+
 }
